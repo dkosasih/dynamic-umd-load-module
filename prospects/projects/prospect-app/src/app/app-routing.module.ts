66ea6a72f7675a33e6bb2df3fabe71dfaacc2445
Deddy Kosasih <deddy.kosasih@carsales.com.au>
2021-08-02 22:30:03 +1000

    feat: add application

---
+++ prospects/projects/prospect-app/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'lib',
+    loadChildren: () => import('../../../prospect-lib/src/lib/prospect-lib.module').then(m => m.ProspectLibModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
